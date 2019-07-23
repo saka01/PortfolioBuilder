fcfc0d732ced2862d373da4caf43082b5095adad
saka01 <sakamoshood@ymail.com>
2019-07-23 08:24:05 -0700

    Saka Edited this files

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 #jijna2.environment is a constructor
 jinja_ev = jinja2.Environment(
@@ -9,15 +11,59 @@ jinja_ev = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+class CssiUser(ndb.Model):
+    first_name = ndb.StringProperty()
+    email = ndb.StringProperty()
+    age = ndb.IntegerProperty()
+
+
 class LoginPage(webapp2.RequestHandler):
     def get(self):
-        login_template = jinja_ev.get_template("Login.html")
-        self.response.write(login_template.render())
+
+        user = users.get_current_user()
+        if user:
+            email_address = user.nickname()
+            logout_url = users.create_logout_url('/')
+            button_dict = {
+                "logout" : logout_url
+            }
+            existing_user = CssiUser.query().filter(CssiUser.email == email_address).get()
+            if existing_user:
+                home_template = jinja_ev.get_template("Home.html")
+                self.response.write(home_template.render())
+            else:
+                register_template = jinja_ev.get_template("Registeration.html")
+                self.response.write(register_template.render(button_dict))
+
+
+        else:
+            login_url = users.create_login_url("/")
+            login_button = '<a href="%s"> Sign In</a>' % login_url
+            # login_button = '<a href="' + login_url + '"> Sign In</a>'
+            self.response.write("Please log in!<br>" + login_button)
+
+def post(Self):
+    user = users.get_current_user()
+
+    cssi_user = CssiUSer(
+        first_name = self.request.get("first_name"),
+        age = int(self.request.get("age")),
+        email = user.nickname()
+    )
+    cssi_user.put()
+    self.response.write("thanks for registering")
+
+class HomePage(webapp2.RequestHandler):
+    def post(self):
+        home_template = jinja_ev.get_template("Home.html")
+        self.response.write(home_template.render())
+
 
 #the app configuration section
 app = webapp2.WSGIApplication(
     [
         ("/", LoginPage),
+        ("/home", HomePage),
 
     ], debug = True
 )
