0ad2b62a78700146cc52576ba83e53668c007205
saka01 <sakamoshood@ymail.com>
2019-07-23 09:09:28 -0700

    saka edited these files

--- appEngine.py
+++ appEngine.py
@@ -1,25 +1,70 @@
-
 import webapp2
+import jinja2
+import os
+from google.appengine.api import users
 from google.appengine.ext import ndb
 
-class User(ndb.Model):
-    userName = ndb.StringProperty(required=True)
-    userDOB= ndb.StringProperty(required=True)
-    userAddress= ndb.StringProperty(required=True)
-    userEmail= ndb.StringProperty(required=True)
+#jijna2.environment is a constructor
+jinja_ev = jinja2.Environment(
+    # /Users/cssi/Desktop/cssi-labs/python/labs/appengine
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
-    def printUserInfo(self):
-        print(self.userName + " " + self.userDOB + " " + self.userAddress + " " + self.userEmail)
+class User(ndb.Model):
+    email = ndb.StringProperty()
+    password = ndb.StringProperty()
+
 class LoginPage(webapp2.RequestHandler):
     def get(self):
-        user1= User(userName= "Smalley", userDOB= "June fifth", userAddress= "0000 Freeland", userEmail= "Smalley2001@gmail.com")
-        user1.printUserInfo()
-        user1.put()
+
+        user = users.get_current_user()
+        print("On login page")
+        if user:
+            print("user is logged in to gmail")
+            email_address = user.nickname()
+            logout_url = users.create_logout_url('/')
+            button_dict = {
+                "logout" : logout_url
+            }
+            existing_user = User.query().filter(User.email == email_address).get()
+            if existing_user:
+                print("user is already registered")
+                home_template = jinja_ev.get_template("Home.html")
+                self.response.write(home_template.render())
+            else:
+                print("user is not registered")
+                register_template = jinja_ev.get_template("Registeration.html")
+                self.response.write(register_template.render(button_dict))
 
 
+        else:
+            login_url = users.create_login_url("/")
+            login_button = '<a href="%s"> Sign In</a>' % login_url
+            # login_button = '<a href="' + login_url + '"> Sign In</a>'
+            self.response.write("Please log in!<br>" + login_button)
+
+def post(Self):
+    user = users.get_current_user()
+
+    resume_user = User(
+        email = user.nickname(),
+        password  = self.request.get("password")
+    )
+    resume_user.put()
+    self.response.write("thanks for registering")
+
+class HomePage(webapp2.RequestHandler):
+    def post(self):
+        home_template = jinja_ev.get_template("Home.html")
+        self.response.write(home_template.render())
+
+
+#the app configuration section
 app = webapp2.WSGIApplication(
     [
         ("/", LoginPage),
+        ("/home", HomePage),
 
     ], debug = True
 )
